0ee6ca254564692e34027575a0402f1c834c6506
Ty Eggen <tycho.eggen@gmail.com>
2022-06-03 20:40:17 -0700

    Improved sample config

--- config.sample.h
+++ config.sample.h
@@ -4,8 +4,18 @@
 
 #define WIFI_SSID "MYWIFI"
 #define WIFI_PASS "MYPASS"
+
 #define MQTT_HOST "test.mosquitto.org"
 #define MQTT_PORT 1883
 #define MQTT_TOPIC "/test/status/space/sergeant"
+
+#define DEFAULT_MODE "network"
+
+#define NUM_LEDS 58
+#define LED_PIN 3
+#define BRIGHTNESS 32
+
+#define REPORTING_CYCLE 500
+#define WATCHDOG_CYCLE 500
 
 #endif
